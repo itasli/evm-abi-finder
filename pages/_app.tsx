bbd9d5465ab02f2a27ac84d85bd922a5689b7871
itasli <ilyas.tasli@outlook.fr>
2022-12-10 12:42:43 +0100

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextNProgress color="#e9e9ed" height={7}/>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
